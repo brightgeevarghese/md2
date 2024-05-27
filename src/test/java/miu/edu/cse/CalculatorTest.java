4e5b7527519ee41048ccc79aade129eda365a1f5
Bright Gee Varghese <brightlsom@gmail.com>
2024-05-27 16:53:48 -0500

    First Commit

---
+++ src/test/java/miu/edu/cse/CalculatorTest.java
@@ -0,0 +1,28 @@
+package miu.edu.cse;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    void testAdd() {
+        int expected = calculator.add(1, 2);
+        assertEquals(expected, 3);
+    }
+
+}
